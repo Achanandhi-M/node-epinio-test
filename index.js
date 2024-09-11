a9e78c9d534eb24407abd43ac81821c35ca5c5b6
Achanandi <achanandhi.m@gmail.com>
2024-09-11 14:42:10 +0530

    Contents-modified

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express=require('express')
 const app=express()
 const port=8080
 app.get('/',(req,res)=>{
-    res.send('I am just NodeJS Application what about you?')
+    res.send('I am just NodeJS Application Hosted on k8s using Epinio what about you?')
 })
 
 app.listen(port,()=>{
